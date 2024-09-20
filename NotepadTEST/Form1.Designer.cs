5d60ec38cf53909274e796e96c8883a5a9f44ded
Danis <danis.safin.99@list.ru>
2024-09-20 15:02:19 +0500

    Удалил кнопка закрытия и покрасил в зеленый фон

--- NotepadTEST/Form1.Designer.cs
+++ NotepadTEST/Form1.Designer.cs
@@ -28,33 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(97, 81);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "Закрыть";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(255, 128, 0);
+            BackColor = Color.Lime;
             ClientSize = new Size(884, 161);
-            Controls.Add(button1);
             Name = "Form1";
             Text = "Блокнот";
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Button button1;
     }
 }
