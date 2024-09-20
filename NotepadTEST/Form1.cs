40999891ec63c96161892749b0ce161eead97cf7
Danis <danis.safin.99@list.ru>
2024-09-20 14:56:44 +0500

    желтый фон и кнопка закрытия

---
+++ NotepadTEST/Form1.cs
@@ -0,0 +1,15 @@
+namespace NotepadTEST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
